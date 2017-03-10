0a59997cd5532fc6049bcd661d3a9c7748d8efca
spickering@tml1.com <NJoyce111174$>
2017-03-10 10:48:07 -0800

    removed .idea added to gitignore

--- span_reader/settings.py
+++ span_reader/settings.py
@@ -1,5 +1,5 @@
-MONGO_CONNSTRxxxxx = 'mongodb://exowriter:qmWSy4K3@10.0.1.2/tmldb?authMechanism=SCRAM-SHA-1'
-MONGO_CONNSTR = 'mongodb://localhost:27017'
+MONGO_CONNSTR = 'mongodb://exowriter:qmWSy4K3@10.0.1.2/tmldb?authMechanism=SCRAM-SHA-1'
+MONGO_CONNSTR_LOCAL = 'mongodb://localhost:27017'
 MONGO_EXO_DB = 'tmldb'
 
 INSTRUMENT_CONFIG_JSON_FILE = './instrumentconfig.json'
