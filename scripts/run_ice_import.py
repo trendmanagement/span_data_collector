9898524d4c8c69bdc6e09dc5aab8c2927fab6681
tmqrwin <spickering@outlook.com>
2017-08-01 08:00:59 -0700

    adjusted run ice import

--- scripts/run_ice_import.py
+++ scripts/run_ice_import.py
@@ -11,12 +11,16 @@ csi = IceSpanImport(None)
 #futures_filepath = "C:/Users/Steve Pickering/Desktop/span_data_collector/ICE_DATA/Sugar Data/EOD_Futures_582_2017.csv"
 #options_filepath = "C:/Users/Steve Pickering/Desktop/span_data_collector/ICE_DATA/Sugar Data/EOD_Options_582_2017.csv"
 
-futures_filepath = "C:/ICE_DATA/EOD_Futures_ProductFile_ProductID(578).csv"
-options_filepath = "C:/ICE_DATA/EOD_Options_578_2016.csv"
+#futures_filepath = "C:/ICE_DATA/EOD_Futures_ProductFile_ProductID(578).csv"
+#options_filepath = "C:/ICE_DATA/EOD_Options_578_2016.csv"
 #futures_filepath = "D:/EOD_Futures_582_2017.csv"
 #options_filepath = "D:/EOD_Options_582_2017.csv"
 
 #futures_filepath = "/home/ubertrader/Downloads/EOD_Futures_ProductFile_ProductID(578).csv"
 #options_filepath = "/home/ubertrader/Downloads/EOD_Options_578_2013.csv"
 
-csi.load_span_file(futures_filepath, options_filepath)
+for year in range(2011,2017+1):
+    futures_filepath = "//10.0.1.4/backup/backups/ICE_DATA/sugar/EOD_Futures_582_{0}.csv".format(year)
+    options_filepath = "//10.0.1.4/backup/backups/ICE_DATA/sugar/EOD_Options_582_{0}.csv".format(year)
+    print(futures_filepath, options_filepath)
+    csi.load_span_file(futures_filepath, options_filepath)
